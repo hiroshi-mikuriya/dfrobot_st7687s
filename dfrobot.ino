6ef1dab47e33e09e3b4e3c3fc5835d4b16096c4a
御厨 洋 <mickly9@MacBookAirHiroshi-3.local>
2019-09-17 22:48:43 +0900

    色を鮮やかにした。画像回転した

--- dfrobot.ino
+++ dfrobot.ino
@@ -14,26 +14,19 @@ uint16_t getPixel(int n, int x, int y) {
   const image_t* img = images[n];
   uint8_t p = pgm_read_byte(img + y0 * IMG_WIDTH + x0);
   uint16_t color = 0;
-  color |= (p & 0xC0) << 8;
-  color |= (p & 0x38) << 5;
-  color |= (p & 0x07) << 2;
+  color |= ((p & 0xC0) | 0x38) << 8;
+  color |= ((p & 0x38) | 0x07) << 5;
+  color |= ((p & 0x07) << 2) | 0x03;
   return color;
 }
 
 void setup(void) {
   Serial.begin(115200);
   lcd.begin();
-}
-
-void loop(void) {
-  static uint16_t colors[] = {0b1111100000000000, 0b1111111111100000,
-                              0b0000011111100000, 0b0000011111111111,
-                              0b0000000000011111, 0b1111100000011111};
-  static int i = 0;
-  i = (i + 1) % (sizeof(colors) / sizeof(*colors));
-  lcd.fillScreen(colors[i]);
   lcd.beforeDraw(0, 0, LCD_WIDTH, LCD_HEIGHT);
   for (int y = 0; y < LCD_HEIGHT; ++y)
     for (int x = 0; x < LCD_WIDTH; ++x) lcd.draw(getPixel(0, x, y));
   lcd.afterDraw();
 }
+
+void loop(void) {}
