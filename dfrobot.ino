7be4a2223488e82c1b43eebc5710dce4fe9ac5f4
hiroshi <hiroshi@iMacHiroshi2.local>
2019-09-24 11:22:08 +0900

    mod

--- dfrobot.ino
+++ dfrobot.ino
@@ -11,16 +11,16 @@ void setup(void) {
 void loop(void) {
   static int n = 0;
   n = (n + 1) % COUNT_OF_IMAGES;
-  const image_t * img = images[n];
+  const image_t* img = images[n];
   uint16_t size = image_sizes[n];
   Lcd.beforeDraw(0, 0, LCD_WIDTH, LCD_HEIGHT);
   for (uint16_t i = 0; i < size; i += 2) {
     uint8_t p = pgm_read_byte(img + i);
     uint8_t count = pgm_read_byte(img + i + 1);
-    uint16_t color = 0;
-    color |= ((p & 0xC0) | 0x38) << 8;
-    color |= ((p & 0x38) | 0x07) << 5;
-    color |= ((p & 0x07) << 2) | 0x03;
+    uint16_t color = 0b0011100011100011;
+    color |= (p & 0xC0) << 8;
+    color |= (p & 0x38) << 5;
+    color |= (p & 0x07) << 2;
     for (int j = 0; j < count; ++j) Lcd.draw(color);
   }
   Lcd.afterDraw();
